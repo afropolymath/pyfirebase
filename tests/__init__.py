5da35a595c18f879740eb20f3773b94436c9e0c7
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-05-31 19:41:37 +0100

    Modifying code to support test coverage

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from .test_firebase import TestFirebase
-
-
-def all_tests():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestFirebase))
-    return suite
